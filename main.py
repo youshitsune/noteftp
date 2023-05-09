5fd3d55c5a4b60414a14ad8302926eb2e002b621
youshitsune <luka@youshitsune.me>
2023-05-09 15:12:13 +0200

    Implement editing and saving a file

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import streamlit as st
 from ftplib import FTP
+import os
 
 if not st.session_state:
     st.session_state.clicked = False
 tmp = st.empty()
 
 def get_files(server):
-    ctx = ["None"]
+    ctx = []
     server.retrlines("NLST", callback=ctx.append)
     return ctx
+
+def get_ctx(ftp, path):
+    ctx = []
+    ftp.retrbinary(f"RETR {path}", ctx.append)
+    for i in range(len(ctx)):
+        try:
+            ctx[i] = ctx[i].decode("utf-8")
+        except Exception:
+            st.error("Can't decode this file")
+            return ""
+            break
+            
+    return "".join(ctx)
+
+def save(ftp, path, ctx):
+    try:
+        ftp.delete(path)
+    except Exception:
+        pass
+    with open("file.txt", "wb") as f:
+        f.write(ctx.encode("utf-8"))
+    ftp.storbinary(f"STOR {path}", open("file.txt", "rb"))
+    os.remove("file.txt")
 
 def run(host, user, passwd):
     ftp = FTP(host, user=user, passwd=passwd)
     files = get_files(ftp)
     file = st.sidebar.selectbox("Select a file:", files)
     with tmp.container():
-        if file == "None":
-            st.write("# Select a file")
-        else:
-            st.write("# You successfully selected a file")
+        ctx = st.text_area(f"{file}", value=get_ctx(ftp, file))
+        if st.button("Save"):
+            save(ftp, file, ctx)
 
 
 if __name__ == "__main__":
